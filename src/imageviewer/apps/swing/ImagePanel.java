532bb68e854fa1878e75fed41021e63f730729a3
Ricardo <Ricardo@DESKTOP-LPK2S35>
2020-12-11 14:48:39 +0000

    (WIK) apps.swing -Image resizing functionality added-

--- src/imageviewer/apps/swing/ImagePanel.java
+++ src/imageviewer/apps/swing/ImagePanel.java
@@ -20,6 +20,22 @@ public class ImagePanel extends JPanel{
     
     @Override
     public void paint (Graphics g){
-        g.drawImage(image, 0, 0, null);
+        Box box = new Box(image.getWidth(), image.getHeight(), this.getWidth(), this.getHeight());
+        g.drawImage(image, box.x, box.y, box.width, box.height, null);
     }          
+
+    private static class Box {
+        final int x;
+        final int y;
+        final int width;
+        final int height;
+        private Box(double imageWidth, double imageHeight, double panelWidth, double panelHeight) {
+            double imageRatio = imageWidth / imageHeight;
+            double panelRatio = panelWidth / panelHeight;
+            this.width = (int) (imageRatio >= panelRatio ? panelWidth : imageWidth * panelHeight / imageHeight);                     
+            this.height = (int) (imageRatio <= panelRatio ? panelHeight : imageHeight * panelWidth / imageWidth);
+            this.x = (int) ((panelWidth - this.width) / 2);
+            this.y = (int) ((panelHeight - this.height) / 2);
+        } 
+    }
 }
