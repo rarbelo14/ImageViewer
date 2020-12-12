b0e9b4a863ff49720da98d3af51a013ddfd9ca96
Ricardo <Ricardo@DESKTOP-LPK2S35>
2020-12-12 13:51:13 +0000

    (WIK) apps.swing -Buttons added-

--- src/imageviewer/apps/swing/Main.java
+++ src/imageviewer/apps/swing/Main.java
@@ -1,22 +1,33 @@
 package imageviewer.apps.swing;
 
+import imageviewer.control.Command;
+import imageviewer.control.NextImageCommand;
+import imageviewer.control.PrevImageCommand;
 import imageviewer.model.Image;
 import imageviewer.view.ImageDisplay;
-import java.awt.PopupMenu;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 
 public class Main extends JFrame {
-    //24:08 Video(2)
+    
+    //CORREGIR UN ERROR EN LA VISUALIZACIÓN DE LA IMAGEN
+    
     public static void main(String[] args) {
         new Main().execute();
     }
     
     private List<Image> images;
     private ImageDisplay imageDisplay;
+    private Map<String, Command> commands = new HashMap<>();
 
     public Main() {
         this.setTitle("Image Viewer");
@@ -24,11 +35,14 @@ public class Main extends JFrame {
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.getContentPane().add(imagePanel());
+        this.add(toolbar(), BorderLayout.SOUTH);
     }
     
     private void execute() {
         this.images = new FileImageLoader(new File("fotos")).load();
         this.imageDisplay.display(images.get(0));
+        this.commands.put("<", new PrevImageCommand(images, imageDisplay));
+        this.commands.put(">", new NextImageCommand(images, imageDisplay));
         this.setVisible(true);
     }
 
@@ -36,5 +50,23 @@ public class Main extends JFrame {
         ImagePanel imagePanel = new ImagePanel();
         this.imageDisplay = imagePanel;
         return imagePanel;
+    }
+
+    private JMenuBar toolbar() {        
+        JMenuBar toolBar = new JMenuBar();
+        toolBar.add(button("<"));
+        toolBar.add(button(">"));
+        return toolBar;
+    }
+
+    private JButton button(String string) {
+        JButton button = new JButton(string);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                commands.get(string).execute();
+            }
+        });
+        return button;
     }
 }
